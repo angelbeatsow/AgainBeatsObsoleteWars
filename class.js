b31b577f024be4ea40d0a68e3fdac00f9ee26a55
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2023-03-13 00:09:38 +0900

    Update class.js

--- class.js
+++ class.js
@@ -13,3 +13,22 @@ const canvas = $('canvas');
 
 
 
+function viewportSet() {
+  var ww = window.innerWidth;
+  var wh = window.innerHeight;
+  
+  var cw = 640;  //canvas.width
+  var ch = 450;  //canvas.height
+  
+  if (ww/wh >= cw/ch) {
+    // widthが長い
+    document.querySelector("meta[name='viewport']").setAttribute("content", "width=" + (640 + (ww - wh*640/450)*450/wh  ) );
+  } else {
+    // それ以外
+    document.querySelector("meta[name='viewport']").setAttribute("content", "width=640");
+  }
+}
+window.addEventListener("DOMContentLoaded", viewportSet, false);
+window.addEventListener("resize", viewportSet, false);
+window.addEventListener("orientationchange", viewportSet, false);
+
